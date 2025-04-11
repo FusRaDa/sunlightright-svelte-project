696629f8903625825b19a3c4732b52fb15668e67
Matthew Rada <matthewr7.christopher@gmail.com>
2025-04-11 18:31:55 -0400

    use vite to somewhat hide google api map key

--- src/lib/components/Sun.svelte
+++ src/lib/components/Sun.svelte
@@ -82,7 +82,7 @@
 
     <div>
       <iframe title="google-maps-embed-api" width="100%" height="300px" style="border:0" loading="lazy" allowfullscreen
-        src="https://www.google.com/maps/embed/v1/place?q={latitude}%2C%20{longitude}&key=AIzaSyByavjM3Zi37I16a0z9BLo6KwgbRAKqaZA">
+        src="https://www.google.com/maps/embed/v1/place?q={latitude}%2C%20{longitude}&key={import.meta.env.VITE_GOOGLE_MAPS_API}">
       </iframe>
     </div>
 
